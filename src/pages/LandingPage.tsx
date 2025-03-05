a17171a6f8c454895570297039e8f779772ec9cd
alexgg9 <alexgalvezgarciaqt@gmail.com>
2025-03-05 14:48:39 +0100

    cambio botones

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 
 const LandingPage = () => {
+
+    const navigate = useNavigate();
+
     return (
         <section className="bg-white bg-opacity-30 py-10 sm:py-14 lg:py-20">
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -17,32 +22,36 @@ const LandingPage = () => {
                     </p>
 
                     <a
-                    href="/register"
-                    className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-12 hover:bg-yellow-400 focus:bg-yellow-400"
-                    >
-                    Registrate
-                    <svg
-                        className="w-6 h-6 ml-8 -mr-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="1.5"
-                        d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                    </svg>
-                    </a>
+                        onClick={() => navigate("/register")}
+                        className="cursor-pointer inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-12 hover:bg-yellow-400 focus:bg-yellow-400"
+                        >
+                        Regístrate
+                        <svg
+                            className="w-6 h-6 ml-8 -mr-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1.5"
+                            d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                        </svg>
+                        </a>
 
-                    <p className="mt-5 text-gray-600">
-                    ¿Ya tienes una cuenta?{" "}
-                    <a href="/login" className="text-black transition-all duration-200 hover:underline">
-                        Inicia sesión
-                    </a>
-                    </p>
+                        <p className="mt-5 text-gray-600">
+                        ¿Ya tienes una cuenta?{" "}
+                        <span
+                            onClick={() => navigate("/login")}
+                            className="cursor-pointer text-black transition-all duration-200 hover:underline"
+                        >
+                            Inicia sesión
+                        </span>
+                        </p>
+
                 </div>
 
                 {/* Imagen de la derecha */}
