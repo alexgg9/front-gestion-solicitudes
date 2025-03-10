0e5c0e9b002839429b3b1732d7f6cb3238ef9f50
alexgg9 <alexgalvezgarciaqt@gmail.com>
2025-03-10 18:40:39 +0100

    cambio api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+    baseURL: "http://docgalvez.duckdns.org:8080/api",
     headers: {
       "Content-Type": "application/json",
     },
